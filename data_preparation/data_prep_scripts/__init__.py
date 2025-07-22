2fd233e446533c8659a9b46095bb95f3fa327dc1
Yehor Mishchyriak <yehormishchyriak@gmail.com>
2025-07-22 15:02:43 -0400

    updated notebooks

--- data_preparation/data_prep_scripts/__init__.py
+++ data_preparation/data_prep_scripts/__init__.py
@@ -5,12 +5,14 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
+UNICLUST_TO_UNIREF_MAP = getenv("UNICLUST2UNIREF_MAP")
 FETCHED_DATA = getenv("FETCHED_DATA")
 RAW_DATA = getenv("RAW_DATA")
 
 __all__ = [
-    "FETCHED_DATA"
-    "RAW_DATA"
+    "UNICLUST_TO_UNIREF_MAP",
+    "FETCHED_DATA",
+    "RAW_DATA",
     # uniprot_parsing
     "unirefs_from_tsv",
     "retrieve_fields_for_unirefs",
