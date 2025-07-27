b059ede81d54b55db4c0aaac28da2ac1f449ff8f
Mishchyriak Yehor <myehor01@login-prod-01.pax.tufts.edu>
2025-07-26 22:11:25 -0400

    upd

--- dataset_curation/for_hpc_use/job.sh
+++ dataset_curation/for_hpc_use/job.sh
@@ -13,16 +13,18 @@
 # have a clean start. purge all loaded modules in current environment
 module purge
 
-module load anaconda/2021.05
+source $HOME/miniconda3/etc/profile.d/conda.sh
 
-source activate gnn_func_annotation
+conda activate gnn_func_annotation
+
+export PYTHONPATH=/cluster/home/myehor01/data_processing/microbiome2function/dataset_curation:$PYTHONPATH
 
 # env vars
 export SAMPLE_FILES=/cluster/tufts/bonhamlab/shared/sequencing/processed/humann/main
 export SAVE_DATA_TO_DIR=/cluster/home/myehor01/data_processing
 export JOB_NAME=$SLURM_JOB_NAME
 
-cd /cluster/home/myehor01/data_processing/microbiome2function/dataset_curation/for_hpc_use
-python data_mining.py
+cd /cluster/home/myehor01/data_processing/microbiome2function/dataset_curation
+python for_hpc_use/data_mining.py
 
 conda deactivate
